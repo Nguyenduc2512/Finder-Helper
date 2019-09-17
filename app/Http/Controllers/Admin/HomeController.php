68b93902e6b2d5d6c4ad157a05b3e0ad1c91b814
luulinh16 <luuthithuylinh182@gmail.com>
2019-09-17 10:41:06 +0700

    login Admin

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -12,12 +12,7 @@ class HomeController extends Controller
 {
     public function index()
     {
-    	$users = DB::table('users')->count();
-    	$admin = DB::table('admins')->count();
-    	$categories = DB::table('categories')->count();
-    	$bv = DB::table('posts')->count();
-    	$posts = DB::table('posts')->where('status', '<>', 1)->count('status');
-         return view('admin.admin', ['users' => $users,'admin' => $admin, 'categories' => $categories, 'posts' => $posts, 'bv' => $bv]);
+         return view('admin.admin');
         // return view('adbvmin.admin');
     }
 

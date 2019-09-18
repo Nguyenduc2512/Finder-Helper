cb80906477a128af453afafad8186d1bd46ff0a1
haiphung1 <haipmph05501@fpt.edu.vn>
2019-09-18 15:29:48 +0700

    request-posts

---
+++ app/Http/Controllers/Admin/PostController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Models\Post;
+use App\Services\RequestPostService;
+use Illuminate\Support\Facades\Config;
+
+class PostController extends Controller
+{
+    protected $requestPost;
+
+    public function __construct(RequestPostService $requestPost)
+    {
+        $this->requestPost = $requestPost;
+    }
+
+    public function index()
+    {
+        $post = $this->requestPost->getAllPostActive();
+        
+        return view('admin.post.list_post', compact('post'));
+    }
+
+    public function requestPost()
+    {
+        $post = $this->requestPost->getAllPostInActive();
+
+        return view('admin.post.request_post', compact('post'));
+    }
+
+    public function edit(Post $post)
+    {
+        return view('admin.post.edit_post', ['post' => $post ]);
+    }
+
+    public function update(Request $request)
+    {
+        $this->requestPost->update($request);
+
+        return redirect()->route('posts.index')->with(['success' => 'Bài đăng đã được duyệt ']);
+    }
+
+    public function destroy(Post $post)
+    {
+        $this->requestPost->destroy($post);
+
+        return $this->requestPost();
+    }
+    
+}

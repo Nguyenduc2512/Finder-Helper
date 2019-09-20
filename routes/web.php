856767db1235461f49e085d74391b42bc535d96a
luulinh16 <luuthithuylinh182@gmail.com>
2019-09-20 13:22:55 +0700

    list user for Admin

--- routes/web.php
+++ routes/web.php
@@ -60,4 +60,7 @@ Route::group(['prefix' => 'posts', 'as' => 'posts.'], function () {
     Route::get('destroy/{post}', 'Admin\PostController@destroy')->name('destroy');
 });
 
+Route::group(['prefix' => 'user1'], function (){
+    Route::get('/list', 'Admin\UserController@index')->name('list-user');
+});
 Auth::routes();
